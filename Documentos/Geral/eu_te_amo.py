3c879e7518517a4d2222139954d3a84b26bdaaac
Tony Denis <tonydenisjunior@gmail.com>
2023-11-26 22:32:01 -0300

    Arquivinho antigo

---
+++ Documentos/Geral/eu_te_amo.py
@@ -0,0 +1,23 @@
+print("\nBem vindo ao jogo do amor, responda três perguntas corretamente para receber uma mensagem especial!\n")
+
+resposta_1 = str(input("Qual foi o local do nosso primeiro beijo?"))
+
+
+if resposta_1 == "botanico" or "jardim botanico":
+    resposta_2 = str(input("\nAcertou!!! \nAgora responda onde eu te pedi oficialmente em namoro? "))
+else:
+    print("Que Pena, não foi dessa vez")
+
+if resposta_2 == "madalosso" or "Madalosso":
+    resposta_3 = str(input("\nAcertou!!! \nAgora responda onde eu te pedi oficialmente em noivado? "))
+else: 
+    print("Que Pena, não foi dessa vez")
+
+if resposta_3 == "baggio" or "pizzaria":
+    resposta_3 = str(input("\nAcertou!!! \n\nVocê eh o amor da minha vida, \ncada dia que passa eu me sinto mais feliz e grato por ter você, \nnao paro um segundo de pensar na vida maravilhosa que teremos juntos. \nAté meu ultimo suspiro eu quero estar com voce. \nTe Amo mais do que tudo!!!"))
+else: 
+    print("Que Pena, não foi dessa vez")
+
+
+
+
